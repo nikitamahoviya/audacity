d438f6a73a241a62931497cb6cfd901ea642f85d
email <mahoviyanikita@gmail.com>
2021-03-23 08:44:25 +0530

    Update Windows Batch scripts to use python3

--- scripts/mw2html_audacity/wiki2htmtemp.bat
+++ scripts/mw2html_audacity/wiki2htmtemp.bat
@@ -3,3 +3,22 @@
 python3 mw2html.py https://alphamanual.audacityteam.org/man ..\..\help\temp -s
 
 
+@echo off
+set "$py=0"
+call:construct
+
+for /f "delims=" %%a in ('python #.py ^| findstr "3"') do set "$py=3"
+del #.py
+goto:%$py%
+
+echo python is not installed or python's path Path is not in the %%$path%% env. var
+exit/b
+
+
+:3
+echo running with PY 3
+
+exit/b
+
+:construct
+echo import sys; print('{0[0]}.{0[1]}'.format(sys.version_info^)^) >#.py
